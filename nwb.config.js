74aa4539565ab9ff296f8ac567465e2a5b975cf6
Clauderic Demers <me@ced.io>
2017-04-12 07:38:22 -0400

    Add prop-types to externals for UMD build

--- nwb.config.js
+++ nwb.config.js
@@ -5,7 +5,8 @@ module.exports = {
     umd: {
       global: 'VirtualList',
       externals: {
-        react: 'React'
+        react: 'React',
+        'prop-types': 'PropTypes',
       }
     }
   },
